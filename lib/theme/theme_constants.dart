6516a2a6a14fe3b7f6e4a00cd04263fcf2a38a63
GucciOniPad <kian27@outlook.de>
2022-11-26 21:45:04 +0100

    failed to change font

--- lib/theme/theme_constants.dart
+++ lib/theme/theme_constants.dart
@@ -4,27 +4,33 @@ import 'package:sani_app/main.dart';
 //Das sind die beiden Theme Settings
 
 ThemeData lightTheme = ThemeData(
+    //fontFamily: 'Din-Condensed',
     backgroundColor: Colors.white,
     appBarTheme: AppBarTheme(
+        centerTitle: true,
         foregroundColor: jet,
         backgroundColor: Colors.white,
         iconTheme: IconThemeData(
           color: jet,
         ),
         titleTextStyle: TextStyle(
+          //fontFamily: 'Din-Condensed',
           color: crayolaBlue,
           fontWeight: FontWeight.bold,
           fontSize: 35,
         )));
 
 ThemeData darkTheme = ThemeData(
+    //fontFamily: 'Din-Condensed',
     brightness: Brightness.dark,
     appBarTheme: AppBarTheme(
+        centerTitle: true,
         iconTheme: IconThemeData(
           color: Colors.white,
         ),
         backgroundColor: jet,
         titleTextStyle: TextStyle(
+          //fontFamily: 'Din-Condensed',
           color: Colors.white,
           fontWeight: FontWeight.bold,
           fontSize: 35,
